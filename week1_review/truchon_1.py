52a9136341edfee17d1307f65a499d31c0349d87
Truchon <katie.truchon@gmail.com>
2024-07-15 10:26:12 -0400

    w1d1 lab1 start

---
+++ week1_review/truchon_1.py
@@ -0,0 +1,14 @@
+#hello this is a comment!
+#Your Name, Lab #, & Date
+#Variable Dictionary 
+#total          the sum total of all grades
+#people         the num of people in the list 
+
+#-------------------------------------------------
+
+#---IMPORTS---------------------------------------
+
+#---FUNCTIONS-------------------------------------
+
+#---MAIN CODE-------------------------------------
+print("Hello World!")
